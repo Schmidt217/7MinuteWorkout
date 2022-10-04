2387b44eeff520e1a4a4ff61d9aafcf4c54ea680
Mike Schmidt <schmidt217@gmail.com>
2022-10-04 09:06:02 -0600

    finished setting up room for workout history data storage

--- app/src/main/java/dev/mikeschmidt/a7minuteworkout/FinishActivity.kt
+++ app/src/main/java/dev/mikeschmidt/a7minuteworkout/FinishActivity.kt
@@ -2,7 +2,12 @@ package dev.mikeschmidt.a7minuteworkout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
 import dev.mikeschmidt.a7minuteworkout.databinding.ActivityFinishBinding
+import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FinishActivity : AppCompatActivity() {
 
@@ -25,5 +30,23 @@ class FinishActivity : AppCompatActivity() {
         binding?.btnFinish?.setOnClickListener {
             finish()
         }
+
+        val dao = (application as WorkoutApp).db.historyDao()
+        addDateToDatabase(dao)
+    }
+
+    private fun addDateToDatabase(historyDao: HistoryDao) {
+
+        val c = Calendar.getInstance()
+        val dateTime = c.time
+        Log.e("date: ", "" +dateTime)
+
+        val sdf = SimpleDateFormat("MMM dd yyyy HH:mm:ss", Locale.getDefault())
+        val date = sdf.format(dateTime)
+        Log.e("Formatted Date: ", "" +date)
+
+       lifecycleScope.launch {
+           historyDao.insert(HistoryEntity(date))
+       }
     }
 }
---
+++ app/src/main/java/dev/mikeschmidt/a7minuteworkout/WorkoutApp.kt
@@ -0,0 +1,9 @@
+package dev.mikeschmidt.a7minuteworkout
+
+import android.app.Application
+
+class WorkoutApp: Application() {
+    val db by lazy {
+        HistoryDatabase.getInstance(this)
+    }
+}
